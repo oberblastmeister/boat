c0a3da8195a53c24b401bdd935be0966952ae275
brian <littlebubu.shu@gmail.com>
2022-02-08 21:22:53 -0500

    .

--- oat-core/src/Oat/Driver.hs
+++ oat-core/src/Oat/Driver.hs
@@ -2,9 +2,12 @@ module Oat.Driver where
 
 import Data.Text.Encoding.Error qualified as Text.Encoding.Error
 import Effectful.Error.Static
+import Effectful.Temporary (Temporary)
+import Effectful.Temporary qualified as Temporary
 import Named (NamedF (Arg), arg, argDef, argF, (!), type (:!), type (:?))
 import Oat.Backend.X86.Codegen qualified as Backend.X86.Codegen
 import Oat.Backend.X86.X86 qualified as Backend.X86
+import Oat.Command (Command)
 import Oat.Command qualified as Command
 import Oat.Common (liftEither, readFileUtf8)
 import Oat.LL qualified as LL
@@ -27,3 +30,15 @@ compileLLTextToInsts :: '[Error [LL.ParseError]] :>> es => Text -> Eff es (Seq B
 compileLLTextToInsts text = do
   prog <- parseLL text
   LL.runNameSource $ Backend.X86.Codegen.compileProg prog
+
+compileAsmFile :: '[Command, Temporary] :>> es => FilePath -> FilePath -> Eff es ()
+compileAsmFile dotS dotO = do
+  Temporary.withSystemTempFile "oat" $ \dotOTemp _handle -> do
+    Command.assemble dotS dotOTemp
+    Command.link [dotOTemp] dotO
+
+compileLLFileClang :: '[Command, Temporary] :>> es => FilePath -> FilePath -> Eff es ()
+compileLLFileClang dotLL dotO = do
+  Temporary.withSystemTempFile "oat" $ \dotSTemp _handle -> do
+    Command.compileLlvm dotLL dotSTemp
+    compileAsmFile dotSTemp dotO
